c247671f34c40fc240000a20d3df8ac58a4f0e95
govind098 <govgukal5@gmail.com>
2023-11-18 10:45:16 +0530

    add calculator

---
+++ calculator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d,e,f;
+    printf("Enter a first Number");
+    scanf("%d",&a);
+    printf("Enter a second Number");
+    scanf("%d",&b);
+    
+    printf("addition of your number is %d\n"a+b);
+    printf("subtraction of your number is %d\n"a-b);
+    printf("multiplication of your number is %d\n"a*b);
+    printf("division of your number is %d\n"a/b);
+
+    
+}
